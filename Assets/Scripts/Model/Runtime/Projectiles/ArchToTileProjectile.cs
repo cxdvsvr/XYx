db5c98b55368b6b9baedfdbf011a304a6bc4f6ae
Roma <xtremegames889@gmail.com>
2025-10-29 01:17:34 +0500

    T2

--- Assets/Scripts/Model/Runtime/Projectiles/ArchToTileProjectile.cs
+++ Assets/Scripts/Model/Runtime/Projectiles/ArchToTileProjectile.cs
@@ -29,12 +29,14 @@ namespace Model.Runtime.Projectiles
             ///////////////////////////////////////
             // Insert you code here
             ///////////////////////////////////////
-
+            totalDistance =( totalDistance / 100 )* 60;
+            float maxHeight = totalDistance;
+            localHeight = maxHeight * (-(t * 2 - 1) * (t * 2 - 1) + 1);
 
             ///////////////////////////////////////
             // End of the code to insert
             ///////////////////////////////////////
-            
+
             Height = localHeight;
             if (time > StartTime + _timeToTarget)
                 Hit(_target);
